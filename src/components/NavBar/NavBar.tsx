d08d01622df1de38608883fb658885a9a1322758
kylectrent <kylectrent@gmail.com>
2025-04-13 20:25:07 -0700

    nav bar styling

--- src/components/NavBar/NavBar.tsx
+++ src/components/NavBar/NavBar.tsx
@@ -1,11 +1,22 @@
+import styles from './NavBar.module.css';
+
 export const NavBar = () => {
   return (
-    <nav>
-      <a href="/">Portfolio</a>
-      <div>
-        <ul>
+    <nav className={styles.navbar}>
+      <a href="/" className={styles.title}>Portfolio</a>
+      <div className={styles.menu}>
+        <ul className={styles.menuItems}>
           <li>
-            <a href="/something"></a>
+            <a href="#about">About</a>
+          </li>
+          <li>
+            <a href="#experience">Experience</a>
+          </li>
+          <li>
+            <a href="#projects">Projects</a>
+          </li>
+          <li>
+            <a href="#contact">Contact</a>
           </li>
         </ul>
       </div>
