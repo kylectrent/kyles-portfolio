2e1230168a54740a96d06c95ff1a4835dbd067cf
kylectrent <kylectrent@gmail.com>
2025-05-17 15:37:55 -0700

    skills icons

--- src/components/experience/Experience.tsx
+++ src/components/experience/Experience.tsx
@@ -1,9 +1,20 @@
+import styles from './Experience.module.css';
+import skills from '../../data/skills.json';
+import { getImageUrl } from '../../utils';
+
 export const Experience = () => {
   return (
     <section id="experience">
       <h2>Experience</h2>
       <div>
-        <div></div>
+        <div>
+          {skills.map((skill) => 
+            <div>
+              {skill.title}
+              <img src={getImageUrl(skill.imageSrc)}/>
+            </div>
+          )}
+        </div>
         <ul></ul>
       </div>
     </section>
