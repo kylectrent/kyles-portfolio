6b49f1dc2d0c6ff136aa54612436c1a1d3bbfba6
kylectrent <kylectrent@gmail.com>
2025-05-17 16:49:42 -0700

    add link to view only resume

--- src/components/experience/Experience.tsx
+++ src/components/experience/Experience.tsx
@@ -1,4 +1,3 @@
-import styles from './Experience.module.css';
 import skills from '../../data/skills.json';
 import { getImageUrl } from '../../utils';
 
@@ -6,14 +5,24 @@ export const Experience = () => {
   return (
     <section id="experience">
       <h2>Experience</h2>
+      <a
+        href="https://1drv.ms/w/c/5dd046f273d700f9/EQQejfl4KmlPvMhLT_-bEBoBdNeTYBk0jolvUNHD4QE2Cw?e=MoKApV"
+        target="_blank"
+        rel="noopener noreferrer"
+        className="resume-link"
+      >
+        View My Resume
+      </a>
       <div>
         <div>
-          {skills.map((skill) => 
-            <div>
-              {skill.title}
-              <img src={getImageUrl(skill.imageSrc)}/>
+          {skills.map((skill, id) => (
+            <div key={id}>
+              <div>
+                {skill.title}
+                <img src={getImageUrl(skill.imageSrc)} />
+              </div>
             </div>
-          )}
+          ))}
         </div>
         <ul></ul>
       </div>
